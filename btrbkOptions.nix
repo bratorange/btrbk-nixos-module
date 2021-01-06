f72302e0f08b8e320059945f87316fc064174108
voidless <juliusjedermann@gmail.com>
2021-01-06 23:43:04 +0100

    added apply functions

--- btrbkOptions.nix
+++ btrbkOptions.nix
@@ -4,10 +4,11 @@ with lib;
 with import ./btrbkHelpers.nix;
 
 let
-  # Since nix has the camel case style convention but the btrbk config options are using snake case, we will need a mapping.
-  optionMappings = {
-     snapshotDir = "snapshot_dir";
-     timestampFormat = "timestamp_format";
+  conversions = {
+    valueIdentityPair = keyName: value:
+      [ (optionalString (value != null) (keyname + "  " + value)) ];
+
+    customLines = value: lines2list value;
   };
 in 
 {
@@ -15,16 +16,21 @@ in
     type = types.nullOr types.str;
     default = null; 
     description = "Directory where snapshots of the filesystem will be stored. Must be given relative to individual volume-directory.";
+    apply = conversions.valueIdentityPair "snapshot_dir";
   };
+
   timestampFormat = mkOption {
     type = types.nullOr (types.enum [ "short" "long" "long-iso" ]);
     default = null;
     description = "Timestamp format used as a suffix for new snapshot modules. 'short' only keeps track of the date, 'long' also tracks the time of day and 'long-iso' will also prevent issues with backups made during a time shift.";
+     apply = conversions.valueIdentityPair "timestamp_format";
   };
+
   extraOptions = mkOption {
     type = with types; nullOr lines;
     default = null;
     description = "Extra options which influence how a backup is stored. See digint.ch/btrbk/doc/btrbk.conf.5.html under 'Options' for more information.";
+    apply = conversions.customLines;
   };
   
 
